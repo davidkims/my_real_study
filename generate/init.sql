94d64d3659ee6648170395a49fae6603a492ac24
davidkims <davidkims@users.noreply.github.com>
2025-05-26 13:31:08 +0000

    자동 배포: 15255233692 / ubuntu-latest / gcc e4561a7256ed7e448732cc2e4e6e47f4861d0611

---
+++ generate/init.sql
@@ -0,0 +1,3 @@
+CREATE TABLE users(id INTEGER PRIMARY KEY, name TEXT, email TEXT);
+INSERT INTO users VALUES(1, 'Alice', 'alice@example.com');
+INSERT INTO users VALUES(2, 'Bob', 'bob@example.com');
