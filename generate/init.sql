ba81babd416dfcebfafc5bcf5007c886a97cc25c
davidkims <davidkims@users.noreply.github.com>
2025-05-29 14:45:54 +0000

    deploy: 69a59186ec6edd9e0e2c2bf082eb631098def894

--- generate/init.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, name TEXT, email TEXT);
-INSERT INTO users VALUES(1, 'Alice', 'alice@example.com');
-INSERT INTO users VALUES(2, 'Bob', 'bob@example.com');
